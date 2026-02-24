a2531075e9b6cb28b3aaa42696d87591750c700e
Asif-ibrahim101 <your.email@example.com>
2026-02-24 17:17:48 +0000

    majority element

---
+++ Mejority_element.py
@@ -0,0 +1,22 @@
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times. 
+# You may assume that the majority element always exists in the array.
+
+#using the boyer moore voting algo for finding the majority element
+
+def solution(nums):
+    candidate = None
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+            count = 1
+        elif candidate == num:
+            count += 1
+        else:
+            count -= 1
+    
+    return candidate
+
+solutions = print(solution([3, 2, 3]))
